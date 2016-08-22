fc79a10c1bcf781af36d0dd024f9c73f23ab2363
olexanders17 <olexanders17@gmail.com>
2016-08-22 10:20:58 +0300

    some changes2

--- 2/app1.js
+++ 2/app1.js
@@ -4,14 +4,15 @@ $(document).ready(function () {
 
     });
 
-    $('#rmm').val('hell444444o');
+   // $('#m1').val('hell444444o');
+    $('#m2').val('sdasd@sfdsf.ty');
 
 
     function onSubmit() {
 //
         //var el = $("form").children('input');
         var el = $("form input").not("[type='submit']");
-        console.log(el );
+        console.log(el);
         el.each(function (index, el) {
             console.log(validate(el.attributes, el));
         })
@@ -24,31 +25,47 @@ $(document).ready(function () {
         // required minlength  maxlength email
 
         var validationRezult = {obj: el};
-        var isForValidation = 'validate-';
+        var validationAttr = 'validate-';
         var result = '';
+        var emailRegExp = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/g;
 
         for (var i = 0; i < attrs.length; i++) {
             var attr = attrs[i];
 
             //check if attrubete needs to be validate
-            if (attr.name.indexOf(isForValidation) < 0) {
+            if (attr.name.indexOf(validationAttr) < 0) {
                 continue;
             }
 
 
             if (attr.name === "validate-required") {
-                validationRezult["validate-required"] = true;
-                if (attr.value == "") {
-                    validationRezult["validate-required"] = false;
+                validationRezult["validate-required"] = false;
+                if (el.value) {
+                    validationRezult["validate-required"] = true;
                 }
             }
 
-            if ( attr.name === "validate-minlength") {
-                validationRezult["validate-minlength"] = true;
+            if (attr.name === "validate-minlength") {
+                validationRezult["validate-minlength"] = false;
                 if ((+attr.value) <= el.value.length) {
-                    validationRezult["validate-minlength"] = false;
+                    validationRezult["validate-minlength"] = true;
                 }
             }
+
+            if (attr.name === "validate-maxlength") {
+                validationRezult["validate-maxlength"] = false;
+                if ((+attr.value) >= el.value.length) {
+                    validationRezult["validate-maxlength"] = true;
+                }
+            }
+
+            if (attr.name === "validate-email") {
+                validationRezult["validate-email"] = false;
+                if (el.value.match(emailRegExp)) {
+                    validationRezult["validate-email"] = true;
+                }
+            }
+
 
         }
 
